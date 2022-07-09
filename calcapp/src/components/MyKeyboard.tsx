df20df5bdfcac224ce63b1206afedeb2c2cbe85c
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-10 00:13:09 +0300

    ketboard

--- calcapp/src/components/MyKeyboard.tsx
+++ calcapp/src/components/MyKeyboard.tsx
@@ -28,6 +28,31 @@ export default function MyKeyboard(){
         setOperation('');
         setResult(null)
     }
+
+    const getResult=()=>{
+        switch(operation){
+            case "+":
+                clear();
+                setResult(parseInt(secondNumber) + parseInt(firstNumber));
+                break;
+            case "-":
+                clear();
+                setResult(parseInt(secondNumber) - parseInt(firstNumber));
+                break;
+            case "*":
+                clear();
+                setResult(parseInt(secondNumber) * parseInt(firstNumber));  
+                break;
+            case "/":
+                clear();
+                setResult(parseInt(secondNumber) / parseInt(firstNumber)); 
+                break;
+            default:
+                clear();
+                setResult(0);
+                break;        
+        }
+    };
     return(
 
     )
