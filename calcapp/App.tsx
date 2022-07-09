94033a91d4cd69efb642982bb826492f9e1bbefd
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-09 23:12:44 +0300

    app

--- calcapp/App.tsx
+++ calcapp/App.tsx
@@ -1,19 +1,18 @@
 import React,{useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Switch } from 'react-native';
+import { StyleSheet, View, Switch } from 'react-native';
 import { ThemeCotext } from './src/context/ThemeCotext';
 
 export default function App() {
   const[theme, setTheme]=useState('light')
   return (
-    <View style={theme === 'light' ? styles.container : [styles.container,{backgroundColor:'#000'}]}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Switch
-        value={theme === 'lighe'}
-        onValueChange={()=>setTheme(theme === 'light' ? 'dark' : 'light')}
-      />
-    </View>
+    <ThemeCotext.Provider value={theme}>
+      <View style={theme === 'light' ? styles.container : [styles.container,{backgroundColor:'#000'}]}>
+          <Switch
+            value={theme === 'lighe'}
+            onValueChange={()=>setTheme(theme === 'light' ? 'dark' : 'light')}
+          />
+      </View>
+    </ThemeCotext.Provider>
   );
 }
 
